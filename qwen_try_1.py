3a7228c5fdabe112be34921e2346ae87063bd84e
ManvithGopu13 <manvithgopu1394@gmail.com>
2025-04-04 20:43:39 +0530

    Ran agent with qwen2.5:7b using CodeAgent

--- qwen_try_1.py
+++ qwen_try_1.py
@@ -1,11 +1,16 @@
 from smolagents import LiteLLMModel
+from smolagents import CodeAgent, HfApiModel
 
 model = LiteLLMModel(
     model_id="ollama_chat/qwen2.5:7b",  # Or try other Ollama-supported models
     api_base="http://127.0.0.1:11434",  # Default Ollama local server
+    api_key="1122",
     num_ctx=8192,
 )
 
-# result = model.("What is the capital of India?")
+agent = CodeAgent(tools=[], model=model, add_base_tools=True)
 
-# print(result)
+agent.run(
+    "Could you give me the 118th number in the Fibonacci sequence?",
+)
+
